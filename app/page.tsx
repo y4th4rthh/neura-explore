dca5750d6e22daee8a69adac88b6ea107cf2dfa6
Patel Yatharth <114477915+y4th4rthh@users.noreply.github.com>
2026-01-10 13:34:50 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
   if (!hasChecked) {
     return (
       <div
-        className="font-sans min-h-screen bg-[#FFFFFF1A]"
+        className="font-sans min-h-screen bg-[#0a0a0a]"
         style={{
           background: wallpaper
             ? `url(${wallpaper}) center/cover no-repeat fixed`
